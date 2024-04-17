a4ccd693de5ff591f879378a86c53b66a02406e1
ocit17 <abdulrosid228@gmail.com>
2024-04-17 22:48:58 +0700

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="about">
+    <h1>Vue Todo Apps</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.about {
+  margin: 10 10 10 10;
+  p {
+    margin: 10 10 10 10;
+  }
+}
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
