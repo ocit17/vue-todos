32ebaaee3ead0adfa34b9f1bc25dc862869a9918
ocit17 <abdulrosid228@gmail.com>
2024-04-17 22:57:25 +0700

    update about

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -5,12 +5,6 @@
 </template>
 
 <style lang="scss" scoped>
-.about {
-  margin: 10 10 10 10;
-  p {
-    margin: 10 10 10 10;
-  }
-}
 @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
